41aa0413652ef09690632c8fe0b9e69f547d0a94
John Smith <nhoxhero073@gmail.com>
2026-04-28 16:40:29 +0700

    Create Categories Part 1

---
+++ frontend/src/features/category/types/category.type.ts
@@ -0,0 +1,14 @@
+export type CategoryType = 'income' | 'expense';
+
+export type Category = {
+    id: string;
+    name: string;
+    type: CategoryType;
+    is_system: boolean;
+    is_deleted: boolean;
+    deleted_at?: string | null;
+    created_at: string;
+    updated_at: string;
+};
+
+export type CategoryTab = 'all' | 'expense' | 'income' | 'deleted';
